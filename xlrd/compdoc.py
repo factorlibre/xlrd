aee011813310ae616f10d240178d0306e3a9c030
Kiko Peiró <francisco.peiro@factorlibre.com>
2022-02-08 12:52:35 +0100

    FIX CompDocError
    https://www.codeforests.com/2020/05/28/fix-the-compdocerror-for-xlrd/

--- xlrd/compdoc.py
+++ xlrd/compdoc.py
@@ -422,9 +422,9 @@ class CompDoc(object):
         tot_found = 0
         found_limit = (expected_stream_size + sec_size - 1) // sec_size
         while s >= 0:
-            if self.seen[s]:
-                print("_locate_stream(%s): seen" % qname, file=self.logfile); dump_list(self.seen, 20, self.logfile)
-                raise CompDocError("%s corruption: seen[%d] == %d" % (qname, s, self.seen[s]))
+            # if self.seen[s]:
+            #     print("_locate_stream(%s): seen" % qname, file=self.logfile); dump_list(self.seen, 20, self.logfile)
+            #     raise CompDocError("%s corruption: seen[%d] == %d" % (qname, s, self.seen[s]))
             self.seen[s] = seen_id
             tot_found += 1
             if tot_found > found_limit:
